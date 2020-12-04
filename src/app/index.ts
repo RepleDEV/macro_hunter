5866c142149b87acbdff1e7b4a54c16dfdf83c96
Gonpachiro Kamaboko <54616266+RepleDEV@users.noreply.github.com>
2020-12-04 16:03:58 +0700

    Initial commit

---
+++ src/app/index.ts
@@ -0,0 +1,28 @@
+import $ from "jquery";
+
+$(() => {
+    showMenu("layout-select")
+    $("button#create").on("click", () => {
+        console.log("Click");
+    });
+    $("button#import").on("click", () => {
+        // 
+    });
+});
+
+/* ---------- MENU FUNCTIONS ----------*/
+
+type Menus = "layout-select";
+
+function switchMenus(menu: Menus) {
+    hideMenu(menu);
+    showMenu(menu);
+}
+
+// Animations will be added later on
+function hideMenu(menu: Menus) {
+    $(`.menu.${menu}`).hide();
+}
+function showMenu(menu: Menus) {
+    $(`.menu.${menu}`).show();
+}
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+/* eslint-disable */
+
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+const rootPath = path.resolve(__dirname);
+const outputPath = path.join(rootPath, "./dist/app/");
+
+module.exports = {
+    resolve: {
+        extensions: [".ts", ".js"],
+        mainFields: ["main", "module", "browser"],
+    },
+    entry: path.join(rootPath, "./src/app/index.ts"),
+    target: "electron-renderer",
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
+            },
+            {
+                test: /\.ts$/,
+                use: ["style-loader", "css-loader", "sass-loader"]
+            }
+        ],
+    },
+    devServer: {
+        contentBase: outputPath,
+        historyApiFallback: true,
+        compress: true,
+        hot: true,
+        port: 4101,
+        publicPath: "/",
+    },
+    output: {
+        filename: "index.js",
+        path: outputPath,
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            filename: "index.html",
+            template: "res/index.html",
+            inject: true,
+            cache: true
+        })
+    ]
+};
